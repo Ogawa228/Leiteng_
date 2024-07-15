94df868ea1f7b513f843ae96f5a14928849611e1
Ogawa228 <65229877+Ogawa228@users.noreply.github.com>
2024-07-15 14:06:25 +0800

    失败

--- ShowTabsInFullscreen/background.js
+++ ShowTabsInFullscreen/background.js
@@ -1,15 +1,28 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ showTabs: true });
-  });
-  
-  chrome.action.onClicked.addListener((tab) => {
-    chrome.storage.sync.get('showTabs', (data) => {
-      const showTabs = !data.showTabs;
-      chrome.storage.sync.set({ showTabs });
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['content.js']
+function updateTabs() {
+  chrome.tabs.query({}, function (tabs) {
+    console.log('Updating tabs:', tabs);
+    for (let tab of tabs) {
+      chrome.tabs.sendMessage(tab.id, { tabs }).catch((error) => {
+        console.error('Error sending message to tab:', tab.id, error);
       });
-    });
+    }
   });
-  
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    updateTabs();
+  }
+});
+chrome.tabs.onRemoved.addListener(updateTabs);
+chrome.tabs.onActivated.addListener(updateTabs);
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log('Received message:', request);
+  if (request.activateTabId) {
+    chrome.tabs.update(request.activateTabId, { active: true });
+  }
+  if (request.closeTabId) {
+    chrome.tabs.remove(request.closeTabId);
+  }
+});
--- ShowTabsInFullscreen/content.js
+++ ShowTabsInFullscreen/content.js
@@ -1,8 +1,68 @@
-chrome.storage.sync.get('showTabs', (data) => {
-    if (data.showTabs) {
-      document.documentElement.style.setProperty('--chrome-tabs-display', 'block');
+document.addEventListener('fullscreenchange', () => {
+    if (document.fullscreenElement) {
+      console.log('Entered fullscreen mode');
+      showTabContainer();
     } else {
-      document.documentElement.style.setProperty('--chrome-tabs-display', 'none');
+      console.log('Exited fullscreen mode');
+      hideTabContainer();
     }
   });
+  
+  chrome.runtime.onMessage.addListener(function (message) {
+    console.log('Received message in content script:', message);
+    if (message.tabs) {
+      updateTabList(message.tabs);
+    }
+  });
+  
+  function showTabContainer() {
+    const tabContainer = document.getElementById('tab-container');
+    if (tabContainer) {
+      tabContainer.style.display = 'block';
+    } else {
+      createTabContainer();
+    }
+  }
+  
+  function hideTabContainer() {
+    const tabContainer = document.getElementById('tab-container');
+    if (tabContainer) {
+      tabContainer.style.display = 'none';
+    }
+  }
+  
+  function createTabContainer() {
+    const container = document.createElement('div');
+    container.id = 'tab-container';
+    container.setAttribute('aria-labelledby', 'tab-button');
+    container.innerHTML = `
+      <div id="tab-button">Tabs</div>
+      <div id="tab-content" aria-labelledby="tab-button" role="tabpanel">
+        <div id="tab-list"></div>
+      </div>
+    `;
+    document.body.appendChild(container);
+  }
+  
+  function updateTabList(tabs) {
+    const tabList = document.getElementById('tab-list');
+    tabList.innerHTML = '';
+    for (let tab of tabs) {
+      const tabBox = document.createElement('div');
+      tabBox.className = 'tab-box';
+      tabBox.innerHTML = `
+        <div class="tab-link" role="button">${tab.title}</div>
+        <div class="tab-close" data-tab-id="${tab.id}" role="button">X</div>
+      `;
+      tabList.appendChild(tabBox);
+  
+      tabBox.querySelector('.tab-link').addEventListener('click', () => {
+        chrome.runtime.sendMessage({ activateTabId: tab.id });
+      });
+  
+      tabBox.querySelector('.tab-close').addEventListener('click', () => {
+        chrome.runtime.sendMessage({ closeTabId: tab.id });
+      });
+    }
+  }
   
